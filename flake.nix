87e6effded466b0f1c0fb8a7957bff3a36170e79
xen <jojoceanus@outlook.com>
2026-02-04 15:18:57 +0800

    inherit inputs

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } : {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 
     nixosConfigurations."ocean" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";

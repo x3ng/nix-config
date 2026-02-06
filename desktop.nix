e011d11cd57bbe05b455cbadc4d1b8c6ab5210bc
xen <jojoceanus@outlook.com>
2026-02-06 19:38:20 +0800

    hyprland: add brightnessctl config

--- desktop.nix
+++ desktop.nix
@@ -14,5 +14,13 @@
     mako
     waybar
     grim
+    brightnessctl
   ];
+
+  security.wrappers.brightnessctl = {
+    source = "${pkgs.brightnessctl}/bin/brightnessctl";
+    capabilities = "cap_sys_rawio+ep";
+    owner = "root";
+    group = "root";
+  };
 }

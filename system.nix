ef76e311625dc09514dea4bb294573a96cc413db
xen <jojoceanus@outlook.com>
2026-02-08 23:20:18 +0800

    add fonts

--- system.nix
+++ system.nix
@@ -61,17 +61,34 @@
 
   # fonts
   fonts.packages = with pkgs; [
+    adwaita-fonts
+    noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
-    sarasa-gothic
     noto-fonts-color-emoji
-    
+    unifont
+    dejavu_fonts
+
+    jetbrains-mono
+    maple-mono.variable
+    hack-font
+    source-code-pro
+    sarasa-gothic
+
     nerd-fonts.symbols-only
     font-awesome
     material-design-icons
   ];
 
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      sansSerif = [ "Noto Sans CJK SC" "Noto Sans" "DejaVu Sans" ];
+      serif = [ "Noto Serif CJK SC" "Noto Serif" "DejaVu Serif" ];
+      monospace = [ "JetBrains Mono" "Maple Mono" "Sarasa Gothic SC" "Hack" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
 
   # user
   users.users.xen = {

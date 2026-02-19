8dbe36e266fe3651be14a88f25a9fec7e764e223
xen <jojoceanus@outlook.com>
2026-02-19 15:37:06 +0800

    home-manager: change emacs to emacs-pgtk

--- home/home.nix
+++ home/home.nix
@@ -12,7 +12,7 @@
 
     wl-clipboard mihomo
 
-    emacs neovim zed-editor calibre thunderbird vlc tigervnc filezilla libreoffice gimp inkscape
+    emacs-pgtk neovim zed-editor calibre thunderbird vlc tigervnc filezilla libreoffice gimp inkscape
 
     alacritty kitty
     

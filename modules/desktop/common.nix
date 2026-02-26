d775c6c73f278a01267de1646e9a96b958af15f0
xen <jojoceanus@outlook.com>
2026-02-26 19:09:56 +0800

    desktop: change display manager from sddm to tuigreet

--- modules/desktop/common.nix
+++ modules/desktop/common.nix
@@ -1,7 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  
-  services.displayManager.sddm.enable = true;
 
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = ''
+          ${pkgs.tuigreet}/bin/tuigreet \
+            --time \
+            --remember \
+            --remember-user-session \
+            --asterisks \
+            --sessions ${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions \
+            --xsessions ${config.services.xserver.displayManager.sessionData.desktops}/share/xsessions
+        '';
+        
+        user = "greeter";
+      };
+    };
+  };
+  
 }

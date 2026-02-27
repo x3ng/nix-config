a0c192aebec9ee192d9f7bbbc7ebc11c9c413d57
xen <jojoceanus@outlook.com>
2026-02-27 15:10:49 +0800

    update input config

--- modules/input/default.nix
+++ modules/input/default.nix
@@ -2,16 +2,13 @@
 
 {
 
-  i18n.inputMethod.enable = true;
-  i18n.inputMethod.type = "fcitx5";
-  i18n.inputMethod.fcitx5.addons = with pkgs; [
-    qt6Packages.fcitx5-chinese-addons
-  ];
-
-  environment.sessionVariables = {
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    XMODIFIERS = "@im=fcitx";
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      qt6Packages.fcitx5-chinese-addons
+      qt6Packages.fcitx5-configtool
+    ];
   };
 
 }

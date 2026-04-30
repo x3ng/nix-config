c3ae03686470e80a75de9959801901da6fa8a564
xen <jojoceanus@outlook.com>
2026-04-30 21:30:36 +0800

    split package.nix from home.nix

--- home/home.nix
+++ home/home.nix
@@ -21,84 +21,8 @@
     enableBashIntegration = true;
   };
 
-  home.packages = with pkgs;[
-    gcc
-    clang-tools
-    python3
-    uv
-    pyright
-    racket
-    ghc
-    cabal-install
-    ormolu
-    haskell-language-server
-
-    ripgrep
-    jq
-    yq-go
-    fzf
-    tmux
-    zellij
-
-    lm_sensors
-    pciutils
-    usbutils
-    git
-    gnumake
-    cmake
-    fastfetch
-    tree
-    gnused
-    gnutar
-    gawk
-    wl-clipboard
-    wget
-    yazi
-    proxychains-ng
-    nmap
-    mihomo
-    texlive.combined.scheme-full
-    android-tools
-    zip
-    unzip
-
-    vim
-    neovim
-    emacs-pgtk
-
-    starship
-
-    alacritty
-    kitty
-    ghostty
-
-    ffmpeg
-    vlc
-    tigervnc
-    filezilla
-    gimp
-    aria2
-    chromium
-    nyxt
-    qutebrowser
-    libreoffice
-    vscode
-    thunderbird
-    xournalpp
-    inkscape
-    kdePackages.kdenlive
-    calibre
-    cherry-studio
-    clash-verge-rev
-    drawio
-    pandoc
-    rustdesk-flutter
-    claude-code
-
-    claude-code
-  ];
-
   imports = [
+    ./packages.nix
     ./modules/xremap/xremap.nix
   ];
 
---
+++ home/packages.nix
@@ -0,0 +1,86 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # dev
+    gcc
+    clang-tools
+    python3
+    uv
+    pyright
+    racket
+    ghc
+    cabal-install
+    ormolu
+    haskell-language-server
+
+    # cli
+    ripgrep
+    jq
+    yq-go
+    fzf
+    tmux
+    zellij
+    git
+    gnumake
+    cmake
+    fastfetch
+    tree
+    gnused
+    gnutar
+    gawk
+    wl-clipboard
+    wget
+    yazi
+
+    # editor
+    vim
+    neovim
+    emacs-pgtk
+
+    # terminal
+    alacritty
+    kitty
+    ghostty
+
+    # browser
+    chromium
+    nyxt
+    qutebrowser
+
+    # media
+    ffmpeg
+    vlc
+    gimp
+    kdePackages.kdenlive
+    inkscape
+
+    # utils
+    libreoffice
+    calibre
+    vscode
+    thunderbird
+    xournalpp
+    drawio
+    pandoc
+    cherry-studio
+    clash-verge-rev
+    claude-code
+
+    # system
+    lm_sensors
+    pciutils
+    usbutils
+    zip
+    unzip
+    proxychains-ng
+    nmap
+    mihomo
+    aria2
+    android-tools
+    tigervnc
+    filezilla
+    rustdesk-flutter
+    texlive.combined.scheme-full
+  ];
+}

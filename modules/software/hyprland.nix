9674c0edaee5c523d74cf8f0a68b72c30e11b8df
xen <jojoceanus@outlook.com>
2026-06-11 18:18:55 +0800

    update hyprland module: greetd, portals, power management, quickshell
    - Replace SDDM with greetd + tuigreet
    - Add xdg-desktop-portal-hyprland + portal-gtk for screen sharing
    - Add UPower for battery monitoring
    - Add logind lid/power key handling
    - Replace rofi with fuzzel, cliphist with clipse
    - Add satty for screenshot annotation
    - Add quickshell, bibata-cursors, adwaita-icon-theme
    - Remove waybar (replaced by quickshell)

--- modules/software/hyprland.nix
+++ modules/software/hyprland.nix
@@ -1,32 +1,89 @@
 { config, pkgs, ... }:
 
 {
-
   programs.hyprland = {
     enable = true;
     withUWSM = true;
     xwayland.enable = true;
   };
 
+  # Display manager
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
+  # Screen sharing + file picker portals
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    config.hyprland = {
+      default = [ "hyprland" "gtk" ];
+    };
+  };
+
+  # Lock screen PAM
+  security.pam.services.hyprlock = {};
+
+  # Power management
+  services.upower.enable = true;
+  services.logind = {
+    settings.Login = {
+      HandleLidSwitch = "suspend";
+      HandleLidSwitchExternalPower = "lock";
+      HandlePowerKey = "suspend";
+    };
+  };
+
   services.blueman.enable = true;
 
   environment.systemPackages = with pkgs; [
+    # Hyprland ecosystem
     hyprpolkitagent
     hyprlock
     hypridle
     hyprshutdown
-    rofi
-    thunar
+
+    # Shell / bar
+    quickshell
+
+    # Launcher + notifications
+    fuzzel
     mako
-    waybar
+
+    # Screenshot + annotation
     grim
     slurp
+    satty
+
+    # Clipboard
+    wl-clipboard
+    clipse
+
+    # Hardware controls
     brightnessctl
     playerctl
-    networkmanagerapplet
     pavucontrol
-    cliphist
-    xdg-desktop-portal-hyprland
+
+    # System tray apps
+    networkmanagerapplet
+
+    # File manager
+    thunar
+
+    # Cursor theme
+    bibata-cursors
+
+    # Icon theme (needed for tray icons)
+    adwaita-icon-theme
   ];
 
   security.wrappers.brightnessctl = {
@@ -35,5 +92,4 @@
     owner = "root";
     group = "root";
   };
-
 }
